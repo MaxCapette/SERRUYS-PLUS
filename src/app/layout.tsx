564263b3ab77abf04244680e7f4e6f9a9bb4a7f9
Dokuyaku <sk8baz@gmail.com>
2024-07-07 13:44:03 +0200

    change meta name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SERRUYS PLUS",
-  description: "Bienvenue sur le site de SERRUYS PLUS",
+  title: "SERRUYS SERVICES",
+  description: "Bienvenue sur le site de SERRUYS SERVICES",
 };
 
 export default function RootLayout({
